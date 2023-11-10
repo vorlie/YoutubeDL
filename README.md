c75c03117190c8d76ca6564d082a6d260d6082db
Charlie <99135647+vorlie@users.noreply.github.com>
2023-11-10 15:02:33 +0100

    Removed Soundcloud downloader from the apps

--- README.md
+++ README.md
@@ -1,28 +1,22 @@
-# Youtube Downloader by Vorlie 
-Simple youtube downloader. 
-Currently supports Windows and MacOS.
-MacOS 11.7.10 and newer.
+# Requirements
+- MacOS 11.7.10 and newer.
+- Or Windows 10+
 
 ## Features
-- Paste youtube link
+- Input the youtube url
 - Select video or audio (mp4/mp3)
-- Select folder (Default is **Windows:** `C:/Users/~/Downloads` or **MacOS:** `/Users/~/Downloads`)
-- Dynamic theme. (Dark/Light depends on the system theme)
+- Select folder (Default: **MacOS:** `/Users/~/Downloads`)
 - Video info viewer
 - And of course download it
-
-# Gallery
-All pictures of the program
-## Dark mode:
-![1](https://cx.tixte.co/r/youtubedl-13.png)
-![2](https://cx.tixte.co/r/youtubedl-14.png)
-![3](https://cx.tixte.co/r/youtubedl-312321.png)
-![4](https://cx.tixte.co/r/youtubedl-213213.png)
-
+  
 # Libraries used to make this possible
 - [PyTube](https://pypi.org/project/pytube/)
 - [Tkinter](https://docs.python.org/3/library/tkinter.html)
-- [CustomTkinter](https://github.com/TomSchimansky/CustomTkinter)
 
 ## Build it yourself
 Use [Pyinstaller](https://pypi.org/project/pyinstaller/)
+
+
+## Gallery
+
+[![1](https://cx.tixte.co/r/youtubedl-macos.png)](https://cx.tixte.co/r/youtubedl-macos.png)
--- youtubedl-macos.py
+++ youtubedl-macos.py
@@ -1,12 +1,19 @@
-import tkinter.messagebox as messagebox
-from tkinter import filedialog
-import customtkinter as ctk
 from pytube import YouTube
 import os
 import sys
+import tkinter.messagebox as messagebox
+from tkinter import filedialog
+from pathlib import Path
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+import subprocess
 
+ASSETS_PATH = Path(sys._MEIPASS) / "assets"
+#ASSETS_PATH = "assets"
 save_directory = os.path.expanduser("~/Downloads/")
 selected_directory = ""
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
 
 def download_video(video_url):
     video = YouTube(video_url)
@@ -45,98 +52,291 @@ def download_audio(video_url):
     print("Audio was downloaded successfully")
     return new_file_path
 
+window = Tk()
+window.title("YoutubeDL v2.0.5 - MacOS")
+window.geometry("850x505")
+window.configure(bg = "#1C1C1E")
 
-class App(ctk.CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("600x330")
-        self.minsize(600, 330)
-        self.title("YoutubeDL v2.0.3 - MacOS")
-        self.resizable(True, True)
-        
-        ctk.set_appearance_mode("System")
-        ctk.set_default_color_theme("blue")
+canvas = Canvas(
+    window,
+    bg = "#1C1C1E",
+    height = 505,
+    width = 850,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
 
-        def select_directory():
-            global selected_directory
-            directory = filedialog.askdirectory(initialdir=selected_directory)
-            selected_directory = directory
-            self.folder_label3.configure(text=f"Selected: {selected_directory}")
+canvas.place(x = 0, y = 0)
+image_image_1 = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(
+    424.0,
+    61.0,
+    image=image_image_1
+)
+
+canvas.create_text(
+    325.0,
+    41.0,
+    anchor="nw",
+    text="YoutubeDL",
+    fill="#FFFFFF",
+    font=("Helvetica Bold", 36 * -1)
+)
+
+image_image_2 = PhotoImage(
+    file=relative_to_assets("image_2.png"))
+image_2 = canvas.create_image(
+    424.0,
+    306.0,
+    image=image_image_2
+)
+
+image_image_3 = PhotoImage(
+    file=relative_to_assets("image_3.png"))
+image_3 = canvas.create_image(
+    297.0,
+    171.0,
+    image=image_image_3
+)
+
+image_image_4 = PhotoImage(
+    file=relative_to_assets("image_4.png"))
+image_4 = canvas.create_image(
+    297.0,
+    305.0,
+    image=image_image_4
+)
+
+image_image_5 = PhotoImage(
+    file=relative_to_assets("image_5.png"))
+image_5 = canvas.create_image(
+    313.0,
+    171.0,
+    image=image_image_5
+)
+
+entry_image_1 = PhotoImage(
+    file=relative_to_assets("entry_1.png"))
+entry_bg_1 = canvas.create_image(
+    307.0,
+    171.5,
+    image=entry_image_1
+)
+entry_1 = Entry(
+    bd=0,
+    bg="#5856D6",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_1.place(
+    x=101.0,
+    y=160.0,
+    width=412.0,
+    height=21.0
+)
+
+image_image_6 = PhotoImage(
+    file=relative_to_assets("image_6.png"))
+image_6 = canvas.create_image(
+    77.0,
+    171.0,
+    image=image_image_6
+)
+
+image_image_7 = PhotoImage(
+    file=relative_to_assets("image_7.png"))
+image_7 = canvas.create_image(
+    689.0,
+    372.0,
+    image=image_image_7
+)
+def download_audio_get():
+    video_url = entry_1.get()
+    if not video_url:
+        messagebox.showinfo("Warning","No youtube URL provided")
+        return
+    file_path = download_audio(video_url)
+
+    if file_path:
+        messagebox.showinfo("Download Complete", f"Audio saved at: {os.path.abspath(file_path)}")
+
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=download_audio_get,
+    relief="flat"
+)
+button_1.place(
+    x=585.0,
+    y=349.0,
+    width=204.0,
+    height=48.0
+)
+
+image_image_8 = PhotoImage(
+    file=relative_to_assets("image_8.png"))
+image_8 = canvas.create_image(
+    689.0,
+    305.0,
+    image=image_image_8
+)
+
+def download_video_get():
+    video_url = entry_1.get()
+    if not video_url:
+        messagebox.showinfo("Warning","No youtube URL provided")
+        return
+    file_path = download_video(video_url)
+
+    if file_path:
+        messagebox.showinfo("Download Complete", f"Video saved at: {os.path.abspath(file_path)}")
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=download_video_get,
+    relief="flat"
+)
+button_2.place(
+    x=586.0,
+    y=282.0,
+    width=204.0,
+    height=48.0
+)
+
+image_image_9 = PhotoImage(
+    file=relative_to_assets("image_9.png"))
+image_9 = canvas.create_image(
+    689.0,
+    238.0,
+    image=image_image_9
+)
 
 
-        self.folder_label = ctk.CTkLabel(self, text="YoutubeDL by vorlie", font=('Arial Black', 20))
-        self.folder_label.grid(row=1,column=0,padx=0, pady=10)
 
-        self.folder_label2 = ctk.CTkLabel(self, text="Video Info:", font=('Arial Black', 20))
-        self.folder_label2.grid(row=1,column=2,padx=0, pady=10)
+image_image_10 = PhotoImage(
+    file=relative_to_assets("image_10.png"))
+image_10 = canvas.create_image(
+    689.0,
+    171.0,
+    image=image_image_10
+)
 
-        self.entry = ctk.CTkEntry(self, placeholder_text="Paste your youtube link here", width=270)
-        self.entry.grid(row=2,column=0,padx=20, pady=5)
+canvas.create_text(
+    61.0,
+    221.0,
+    anchor="nw",
+    text="Video Information:",
+    fill="#FFFFFF",
+    font=("Helvetica Bold", 24 * -1)
+)
 
-        def audio_callback(choice):
-            print("optionmenu dropdown clicked:", choice)
+video_info = canvas.create_text(
+    61.0,
+    265.0,
+    anchor="nw",
+    text="Title:\nUploader:\nDuration:\nViews:",
+    fill="#FFFFFF",
+    font=("Helvetica", 20 * -1)
+)
+def fetch_video_info():
+    video_url = entry_1.get()
+    if not video_url:
+        messagebox.showinfo("Warning","No youtube URL provided")
+        return
 
-        self.audio_var = ctk.StringVar(value="Audio (.mp3)")
-        self.audio = ctk.CTkOptionMenu(self, values=["Video 720p (.mp4)", "Audio (.mp3)"], command=audio_callback, variable=self.audio_var, width=270)
-        self.audio.grid(row=3, column=0,pady=5)
+    yt = YouTube(video_url)
+    try:
+        title = yt.title
+        uploader = yt.author
+        views = yt.views
+        length_seconds = yt.length
+        length_minutes = length_seconds // 60
+        length_seconds = length_seconds % 60
+        length_formatted = f"{length_minutes}:{length_seconds:02d}"
+    except Exception as e:
+        print(f"Error fetching video info: {e}")
+        return
 
-        def fetch_video_info():
-            video_url = self.entry.get()
-            if not video_url:
-                print("No video URL provided")
-                return
+    canvas.itemconfigure(video_info, text=f"Title: {title}\nUploader: {uploader}\nDuration: {length_formatted}\nViews: {views}")
 
-            yt = YouTube(video_url)
-            try:
-                title = yt.title
-                uploader = yt.author
-                views = yt.views
-                publish_date = yt.publish_date
-                length_seconds = yt.length
-                length_minutes = length_seconds // 60
-                length_seconds = length_seconds % 60
-                length_formatted = f"{length_minutes}:{length_seconds:02d}"
-            except Exception as e:
-                print(f"Error fetching video info: {e}")
-                return
-            
+button_image_4 = PhotoImage(
+    file=relative_to_assets("button_4.png"))
+button_4 = Button(
+    image=button_image_4,
+    borderwidth=0,
+    highlightthickness=0,
+    command=fetch_video_info,
+    relief="flat"
+)
+button_4.place(
+    x=586.0,
+    y=148.0,
+    width=204.0,
+    height=48.0
+)
+image_image_11 = PhotoImage(
+    file=relative_to_assets("image_11.png"))
+image_11 = canvas.create_image(
+    78.0,
+    62.0,
+    image=image_image_11
+)
 
-            self.video_title.configure(text=f"Title: {title}\nUploader: {uploader}")
-            self.video_stats.configure(text=f"Duration: {length_formatted}\nViews: {views} \nPublished: {publish_date}")
+canvas.create_text(
+    125.0,
+    41.0,
+    anchor="nw",
+    text="Vorlie",
+    fill="#FFFFFF",
+    font=("Helvetica Bold", 36 * -1)
+)
 
-        self.fetch_button = ctk.CTkButton(self, text="Fetch Video Info", command=fetch_video_info, width=270)
-        self.fetch_button.grid(row=4, column=0, pady=5)
+image_image_12 = PhotoImage(
+    file=relative_to_assets("image_12.png"))
+image_12 = canvas.create_image(
+    425.0,
+    439.0,
+    image=image_image_12
+)
 
-        self.button = ctk.CTkButton(self, text="Select Folder", command=select_directory, width=270)
-        self.button.grid(row=5, column=0,pady=5)
+dir_text = canvas.create_text(
+    61.0,
+    425.0,
+    anchor="nw",
+    text=f"Dir: {save_directory}",
+    fill="#FFFFFF",
+    font=("Helvetica", 20 * -1)
+)
+def select_directory():
+    global selected_directory
+    directory = filedialog.askdirectory(initialdir=selected_directory)
+    selected_directory = directory
+    canvas.itemconfigure(dir_text, text=f"Dir: {selected_directory}")
 
-        def download():
-            video_url = self.entry.get()
-            is_audio = self.audio_var.get()
+button_image_3 = PhotoImage(
+    file=relative_to_assets("button_3.png"))
+button_3 = Button(
+    image=button_image_3,
+    borderwidth=0,
+    highlightthickness=0,
+    command=select_directory,
+    relief="flat"
+)
+button_3.place(
+    x=586.0,
+    y=214.0,
+    width=206.0,
+    height=48.0
+)
 
-            if not video_url:
-                messagebox.showerror("Error", "Please provide a YouTube link")
-                return
-
-            if is_audio == "Audio (.mp3)":
-                file_path = download_audio(video_url)
-            else:
-                file_path = download_video(video_url)
-
-            if file_path:
-                messagebox.showinfo("Download Complete", f"File saved at: {os.path.abspath(file_path)}")
-
-        self.button = ctk.CTkButton(self, text="Download", command=download, width=270)
-        self.button.grid(row=6, column=0, pady=5)
-
-        self.folder_label3 = ctk.CTkLabel(self, text=f"Default: {save_directory}")
-        self.folder_label3.grid(row=7,column=0)
-
-        self.video_title = ctk.CTkLabel(self, text="No title/uploader")
-        self.video_title.grid(row=2,column=2)
-
-        self.video_stats = ctk.CTkLabel(self, text="No stats")
-        self.video_stats.grid(row=3,column=2)
-
-app = App()
-app.mainloop()
+window.resizable(False, False)
+window.mainloop()
